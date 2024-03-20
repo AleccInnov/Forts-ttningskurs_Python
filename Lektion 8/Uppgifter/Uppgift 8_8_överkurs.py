edcd22838deec3c1ed939b6b30ad58b8d4b3f2a9
AleccInnov <alexandra@astragal.se>
2024-03-20 15:06:48 +0100

    Ny text överkursuppg

--- Lektion 8/Uppgifter/Uppgift 8_8_överkurs.py
+++ Lektion 8/Uppgifter/Uppgift 8_8_överkurs.py
@@ -1,3 +1,3 @@
-#Fortsätt med koden och filerna från uppgift 8_4
+#Fortsätt med koden och filerna från uppgift 8_7
 #Om det bara finns en röd boll kvar så raderas inte den bollen
 #även om man klickar på en grön boll
