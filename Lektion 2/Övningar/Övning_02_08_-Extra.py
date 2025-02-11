b02faac77b86a7c51c3968da02dd152e1a9a96f1
MisaITJohan <johan.marmen@misa.se>
2025-02-11 11:37:24 +0100

    Förtydligar instruktionen i Övning_02_08_-Extra.py lite grann.

--- Lektion 2/Övningar/Övning_02_08_-Extra.py
+++ Lektion 2/Övningar/Övning_02_08_-Extra.py
@@ -1,2 +1,2 @@
 # Jobba vidare i samma fil som i övning 7.
-# Lägg till en funktion som tar ut all data från tabellen och visar upp den.
+# Lägg till en funktion som tar ut all data från tabellen "users" och visar upp den.
