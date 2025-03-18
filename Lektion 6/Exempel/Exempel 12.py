e47b14a9e4c8d4c1956f9a140f455e0cc7e38eb2
AleccInnov <alexandra@astragal.se>
2025-03-18 11:24:44 +0100

    Update Exempel 12.py
    Textjustering

--- Lektion 6/Exempel/Exempel 12.py
+++ Lektion 6/Exempel/Exempel 12.py
@@ -4,14 +4,14 @@ formatted_float = "Flyttal: {:.2f}".format(float_number)
 
 # Sträng formateras med en viss bredd och är vänsterjusterad
 string_value = "Python"
-formatted_string_left = "Sträng (vänsterjusterad): {:<10}".format(string_value)
+formatted_string_left = "{:<10}Sträng (vänsterjusterad): ".format(string_value)
 
 # Sträng formateras med en viss bredd och är centrerad
-formatted_string_center = "Sträng (centrerad): {:^10}".format(string_value)
+formatted_string_center = "{:^10}Sträng (centrerad): ".format(string_value)
 
 # Heltal formateras med en viss bredd och är högerjusterad
 integer_number = 42
-formatted_integer = "Heltal (högerjusterad): {:10d}".format(integer_number)
+formatted_integer = "{:10d}Heltal (högerjusterad): ".format(integer_number)
 
 # Skriv ut de formaterade strängarna
 print(formatted_float)
