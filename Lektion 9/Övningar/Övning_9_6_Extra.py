afe010d9b5590ea6bebe0b51ac654666cc5b1897
AleccInnov <alexandra@astragal.se>
2025-04-01 10:12:17 +0200

    Update Övning_9_6_Extra.py

--- Lektion 9/Övningar/Övning_9_6_Extra.py
+++ Lektion 9/Övningar/Övning_9_6_Extra.py
@@ -1,3 +1,3 @@
-# Använd koden från Exempel 8_8 eller från din egen kod från förra veckan.
+# Använd koden från Exempel 9_11 eller från din egen kod från förra veckan.
 # Lägg till kod så att det finns en variabel som lägger till 1 poäng varje gång
 # man klickar på en boll.
